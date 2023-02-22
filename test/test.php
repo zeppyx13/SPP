2f9eb0e700b7a9380f771d3bd7ec4b3e923142d3
zeppyx13 <gungnands@gmail.com>
2023-02-22 22:22:23 +0800

    table

--- test/test.php
+++ test/test.php
@@ -13,8 +13,8 @@ $nama = $_SESSION['nama'];
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="../style/asidenav.css">
-    <link rel="stylesheet" href="../style/table.css">
+    <link rel="stylesheet" href="style.css">
+    <link rel="stylesheet" href="../style/dashboardadmin.css">
     <title>Document</title>
 </head>
 
