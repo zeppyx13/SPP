626363cfd3780b1d6bd1050595b3cb1119b45cb7
zeppyx13 <gungnands@gmail.com>
2023-02-25 16:57:49 +0800

    change icon

--- admin/tarif.php
+++ admin/tarif.php
@@ -50,7 +50,9 @@ $no = 1;
                     </li>
                     <li>
                         <a class="active" href="./tarif.php">
-                            <img src="../assets/emoticon/svg/regular/bx-table.svg" style="width:15px; height:15px; margin-right:5px;">
+                            <svg xmlns="http://www.w3.org/2000/svg" fill="curentcolor" viewBox="0 0 24 24">
+                                <path d="M4 21h15.893c1.103 0 2-.897 2-2V5c0-1.103-.897-2-2-2H4c-1.103 0-2 .897-2 2v14c0 1.103.897 2 2 2zm0-2v-5h4v5H4zM14 7v5h-4V7h4zM8 7v5H4V7h4zm2 12v-5h4v5h-4zm6 0v-5h3.894v5H16zm3.893-7H16V7h3.893v5z" />
+                            </svg>
                             <b>Tabel SPP</b>
                         </a>
                     </li>
