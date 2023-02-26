85b0b99b6edfa283a454c7482aed23214ad00212
zeppyx13 <gungnands@gmail.com>
2023-02-26 13:04:10 +0800

    gtw gw

--- test/test.php
+++ test/test.php
@@ -14,8 +14,8 @@ $query = ""
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="style.css">
-    <link rel="stylesheet" href="../style/dashboardadmin.css">
+    <link rel="stylesheet" href="../style/table.css">
+    <link rel="stylesheet" href="../style/asidenav.css">
     <title>Document</title>
 </head>
 
