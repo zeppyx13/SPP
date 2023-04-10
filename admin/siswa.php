64ef5be6d3d7cfda6c69fd52c4ddc8b8eb7831d3
zeppyx13 <gungnands@gmail.com>
2023-04-10 08:29:18 +0800

    improved siswa hapus

--- admin/siswa.php
+++ admin/siswa.php
@@ -146,12 +146,26 @@ $no = 1;
                             <td><?= $row["Tlp"] ?></td>
                             <td><?= $row["Angkatan"] ?></td>
                             <td><?= $row["Nominal"] ?></td>
-                            <td><a onclick="return confirm('Yakin ingin menghapus siswa : <?= $row['Nama_Siswa'] ?>')" class="icon-hps" href="../config/action/delete/all.php?id=<?= $row['Nis']; ?>&tabel=siswa&primary=Nis">
-                                    <svg xmlns="http://www.w3.org/2000/svg" fill="curentcolor" viewBox="0 0 24 24">
-                                        <path d="M5 20a2 2 0 0 0 2 2h10a2 2 0 0 0 2-2V8h2V6h-4V4a2 2 0 0 0-2-2H9a2 2 0 0 0-2 2v2H3v2h2zM9 4h6v2H9zM8 8h9v12H7V8z" />
-                                        <path d="M9 10h2v8H9zm4 0h2v8h-2z" />
+                            <td>
+                                <?php
+                                $id_cek = $row['Nis'];
+                                $query = mysqli_query($konek, "SELECT siswa.Nis, pembayaran.Nis FROM siswa INNER JOIN pembayaran on siswa.Nis=pembayaran.Nis WHERE siswa.Nis='$id_cek'");
+                                $cek = mysqli_num_rows($query);
+                                if ($cek == 0) {
+                                ?>
+                                    <a onclick="return confirm('Yakin ingin menghapus siswa : <?= $row['Nama_Siswa'] ?>')" class="icon-hps" href="../config/action/delete/all.php?id=<?= $row['Nis']; ?>&tabel=siswa&primary=Nis">
+                                        <svg xmlns="http://www.w3.org/2000/svg" fill="curentcolor" viewBox="0 0 24 24">
+                                            <path d="M5 20a2 2 0 0 0 2 2h10a2 2 0 0 0 2-2V8h2V6h-4V4a2 2 0 0 0-2-2H9a2 2 0 0 0-2 2v2H3v2h2zM9 4h6v2H9zM8 8h9v12H7V8z" />
+                                            <path d="M9 10h2v8H9zm4 0h2v8h-2z" />
+                                        </svg>
+                                    </a>
+                                <?php } else { ?>
+                                    <svg class="icon-hps" xmlns="http://www.w3.org/2000/svg" fill="curentcolor" viewBox="0 0 24 24">
+                                        <path d="M7 10h10v4H7z" />
+                                        <path d="M12 2C6.486 2 2 6.486 2 12s4.486 10 10 10 10-4.486 10-10S17.514 2 12 2zm0 18c-4.411 0-8-3.589-8-8s3.589-8 8-8 8 3.589 8 8-3.589 8-8 8z" />
                                     </svg>
-                                </a>
+                                <?php } ?>
+
                                 <a class="icon-edit" href="../config/action/update/siswa.php?id=<?= $row["Nis"] ?>">
                                     <svg xmlns="http://www.w3.org/2000/svg" fill="curentcolor" viewBox="0 0 24 24">
                                         <path d="M18.404 2.998c-.757-.754-2.077-.751-2.828.005l-1.784 1.791L11.586 7H7a.998.998 0 0 0-.939.658l-4 11c-.133.365-.042.774.232 1.049l2 2a.997.997 0 0 0 1.049.232l11-4A.998.998 0 0 0 17 17v-4.586l2.207-2.207v-.001h.001L21 8.409c.378-.378.586-.881.585-1.415 0-.535-.209-1.038-.588-1.415l-2.593-2.581zm-3.111 8.295A.996.996 0 0 0 15 12v4.3l-9.249 3.363 4.671-4.671c.026.001.052.008.078.008A1.5 1.5 0 1 0 9 13.5c0 .026.007.052.008.078l-4.671 4.671L7.7 9H12c.266 0 .52-.105.707-.293L14.5 6.914 17.086 9.5l-1.793 1.793zm3.206-3.208-2.586-2.586 1.079-1.084 2.593 2.581-1.086 1.089z" />
